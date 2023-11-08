1819d8db4c50cd41806831ecd5122b19a8ae9d98
matt <matthewthomsen99@gmail.com>
2023-11-07 22:56:02 -0600

    add translate function

--- mazer.go
+++ mazer.go
@@ -16,13 +16,13 @@ type Grid struct {
     width int
 }
 
+var directions = [][]int{ {-2, 0}, {2, 0}, {0, -2}, {0, 2} }
 
 func main() {
     var height = 5 
     var width = 5
     var blocked string = "#"
     var passage string = " "
-    //var frontier_directions = [][]int{ {-1, 0}, {2, 0}, {0, -2}, {0, 2} }
     var grid Grid
     var cells = make([][]Cell, width+1) 
 
@@ -49,10 +49,12 @@ func main() {
    
     // Compute its frontier cells. 
     // A frontier cell of a Cell is a cell with distance 2 in state Blocked and within the grid.\
-    //var frontier = []Cell{}
-    //for i := 0; i <= len(frontier_directions); i++ {}
-    test_cell := grid.cells[3][3]
-    fmt.Println(test_cell.coord)
+    test_cell := grid.cells[3][3].coord
+    var move = []int{-2, 0}
+    var test_trans = translate_slice(test_cell, move)
+    fmt.Println(test_trans)
+
+    //fmt.Println(find_frontier(test_cell))
 
     // 3. While the list of frontier cells is not empty:
         //Pick a random frontier cell from the list of frontier cells.
@@ -61,6 +63,36 @@ func main() {
         //Compute the frontier cells of the chosen frontier cell and add them to the frontier list. 
         //Remove the chosen frontier cell from the list of frontier cells.
 }
+
+func find_frontier(current []int) int {
+    var fcells = []int{}
+    
+    for _, value := range directions {
+        fmt.Println(value)
+    }
+
+    fmt.Println(fcells)
+    return 0
+    // initial checks
+    // frontier cell is within grid
+   
+    // frontier cell state = blocked
+}
+
+func translate_slice(input, qty []int) []int {
+    var new_slice = []int{}
+    for i, _ := range qty  {
+        new_slice = append(new_slice, input[i]+qty[i])
+    }
+    return new_slice
+}
+
+
+
+
+
+
+
 
 
 
